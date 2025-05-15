f82d3279fe7bae51e3b95f85b2d11da490407190
Abdel Hakim Gafer <abdelhakimelazaly4@gmail.com>
2025-05-15 12:42:38 +0300

    Add MERN Booking App with README by Abdel Hakim Gafer

---
+++ .history/frontend/src/components/Header_20250514140819.tsx
@@ -0,0 +1,45 @@
+import { Link } from "react-router-dom";
+import { useAppContext } from "../contexts/AppContext";
+import SignOutButton from "./SignOutButton";
+
+const Header = () => {
+  const { isLoggedIn } = useAppContext();
+
+  return (
+    <div className="bg-[#181818] py-6">
+      <div className="container mx-auto flex justify-between">
+        <span className="text-3xl text-[#99FFB8] font-bold tracking-tight">
+          <Link to="/">Coder With Go Lang</Link>
+        </span>
+        <span className="flex space-x-2">
+          {isLoggedIn ? (
+            <>
+              <Link
+                className="flex items-center text-white px-3 font-bold hover:bg-black"
+                to="/my-bookings"
+              >
+                My Bookings
+              </Link>
+              <Link
+                className="flex items-center text-white px-3 font-bold hover:bg-black"
+                to="/my-hotels"
+              >
+                My Hotels
+              </Link>
+              <SignOutButton />
+            </>
+          ) : (
+            <Link
+              to="/sign-in"
+              className="flex bg-white items-center text-black px-3 font-bold hover:bg-gray-100"
+            >
+              Sign In
+            </Link>
+          )}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ .history/frontend/src/components/Hero_20250515120615.tsx
@@ -0,0 +1,14 @@
+const Hero = () => {
+  return (
+    <div className="bg-gray-800 pb-16">
+      <div className="container mx-auto flex items-center flex-col gap-2">
+        <h1 className="text-[20px] text-[#ff0984] font-bold">Find your next stay</h1>
+        <p className="text-sm text-[#ff0984] font">
+          Search low prices on hotels for your dream vacation...
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
